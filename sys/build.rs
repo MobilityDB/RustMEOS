e6f1dea4ff107a22444fd8c02b93cb66b4cf9385
David García Morillo <davidgmorillop@gmail.com>
2024-07-30 21:48:28 +0200

    Improved build script and add more documentation

--- sys/build.rs
+++ sys/build.rs
@@ -1,8 +1,20 @@
-fn main() {
-    // Tell cargo to look for shared libraries in the specified directory
-    println!("cargo:rustc-link-search=/usr/local/lib/");
+use std::env;
+use std::ffi::OsString;
 
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-env-changed=MEOS_LIB_DIR");
+
+    let lib_dir_env = env::var_os("MEOS_LIB_DIR")
+        .map(OsString::into_string)
+        .transpose()
+        .ok()
+        .flatten()
+        .unwrap_or(String::from("/usr/local/lib/"));
+
+    // Tell cargo to look for shared libraries in the specified directory
+    println!("cargo:rustc-link-search={lib_dir_env}");
+
+    // Tell cargo to tell rustc to link the system meos shared library.
     println!("cargo:rustc-link-lib=dylib=meos");
 }
--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -1,3 +1,14 @@
+//! Low level [MEOS](https://libmeos.org/) C API bindings for MEOS >= 1.1.
+//!
+//! It provides C-interface as is. If you want to use a more Rust-friendly crate,
+//! use the [meos](https://github.com/MobilityDB/RustMEOS) crate.
+
+//! You can also find it on [crates.io](https://crates.io/crates/meos).
+//!
+//! The build will use system-installed MEOS if available.
+//!
+//! This documentation is generated based on MEOS 1.1.
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
